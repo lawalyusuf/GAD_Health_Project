6d523b9d358c2d9b18d42a58a0d8cc094d12948b
Yusuf <bwitlawalyusuf@gmail.com>
2021-12-10 12:17:44 +0100

    node engine

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/ervy-ng'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<ervy-ng>'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/ervy-ng/index.html'));});
+    
+res.sendFile(path.join(__dirname+'/dist/<ervy-ng>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
